cd0085393d40a981f1c1d3883ad51075867310e3
Toolkit User <issam.laradji@gmail.com>
2020-09-23 15:39:37 +0000

    latest

--- scripts/process_shapenet.py
+++ scripts/process_shapenet.py
@@ -2,8 +2,7 @@ import os
 import trimesh
 import glob
 
-
-def create_trimesh_from_shapenet_obj(obj_path, which_mesh=0):
+def get_tmesh(obj_path, which_mesh=0):
   scene_or_mesh = trimesh.load_mesh(obj_path, process=False)
 
   if isinstance(scene_or_mesh, trimesh.Scene):
@@ -13,7 +12,9 @@ def create_trimesh_from_shapenet_obj(obj_path, which_mesh=0):
     tmesh = mesh_list[which_mesh]
   else:
     tmesh = scene_or_mesh
-  
+  return tmesh 
+
+def verify_tmesh(tmesh):
   # Sanity checks
   if tmesh.is_empty:
     raise ValueError("Mesh is empty!")
@@ -24,17 +25,43 @@ def create_trimesh_from_shapenet_obj(obj_path, which_mesh=0):
   if tmesh.body_count > 1:
     raise ValueError("Mesh consists of more than one connected component (bodies)!")
 
-  return tmesh
 
+def save_image(fname, src_fname):
+    from stl import mesh
+    from mpl_toolkits import mplot3d
+    from matplotlib import pyplot
+
+    # Create a new plot
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+
+    # Load the STL files and add the vectors to the plot
+    your_mesh = mesh.Mesh.from_file(src_fname)
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+
+    # Auto scale to the mesh size
+    scale = your_mesh.points.flatten('F')
+    axes.auto_scale_xyz(scale, scale, scale)
+    pyplot.savefig(fname)
+    print('Saved')
 
 if __name__ == "__main__":
+    
     datadir = '/mnt/public/datasets/ShapeNetCore.v2/02691156'
 
     obj_hash_list = os.listdir(datadir)
     for i, obj_hash in enumerate(obj_hash_list):
         obj_path = os.path.join(datadir, obj_hash, 'models', 'model_normalized.obj')
+        tmesh = get_tmesh(obj_path)
+
+        # save png for visualization
+        src_fname = '.tmp/%s.stl' % obj_hash
+        out_fname = '.tmp/%s.png' % obj_hash
+        tmesh.export(src_fname)
+        save_image(out_fname, src_fname)
+
         try:
-            mesh = create_trimesh_from_shapenet_obj(obj_path, which_mesh=0)
-            print('good!')
+            verify_tmesh(tmesh)
         except ValueError as e:
             print(i, '-', e)
+        
