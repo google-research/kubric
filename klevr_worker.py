cb731da3e9d6a77280cd1a4bc045a67fd559f3d2
Klaus Greff <klausg@google.com>
2020-10-02 12:52:52 +0200

    refactored worker interface to reduce boilerplate

---
+++ klevr_worker.py
@@ -0,0 +1,138 @@
+# Copyright 2020 The Kubric Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datetime
+
+import sys; sys.path.append(".")
+
+import kubric.pylab as kb
+
+
+class KLEVRWorker(kb.Worker):
+  def get_argparser(self):
+    parser = super().get_argparser()
+    # add additional commandline arguments
+    parser.add_argument("--min_nr_objects", type=int, default=4)
+    parser.add_argument("--max_nr_objects", type=int, default=10)
+    # overwrite default values
+    parser.set_defaults(asset_source=['./Assets/KLEVR'])
+    return parser
+
+
+  def add_floor(self):
+    floor = self.create_asset("KLEVR", asset_id="Floor", static=True,
+                              position=(0, 0, -0.2), scale=(2, 2, 2))
+    self.add(floor)
+    return floor
+
+  def add_lights(self):
+    # --- Light settings from CLEVR
+    sun = kb.DirectionalLight(color=kb.get_color("white"), intensity=0.45,
+                              shadow_softness=0.2,
+                              position=(11.6608, -6.62799, 25.8232))
+    sun.look_at((0, 0, 0))
+    lamp_back = kb.RectAreaLight(color=kb.get_color("white"), intensity=50.,
+                                 position=(-1.1685, 2.64602, 5.81574))
+    lamp_back.look_at((0, 0, 0))
+    lamp_key = kb.RectAreaLight(color=kb.get_color(0xffedd0), intensity=100,
+                                width=0.5, height=0.5,
+                                position=(6.44671, -2.90517, 4.2584))
+    lamp_key.look_at((0, 0, 0))
+    lamp_fill = kb.RectAreaLight(color=kb.get_color(0xc2d0ff), intensity=30,
+                                 width=0.5, height=0.5,
+                                 position=(-4.67112, -4.0136, 3.01122))
+    lamp_fill.look_at((0, 0, 0))
+    self.add(sun, lamp_back, lamp_key, lamp_fill)
+
+  def add_camera(self):
+    camera = kb.PerspectiveCamera(focal_length=35., sensor_width=32,
+                                  position=(7.48113, -6.50764, 5.34367))
+    camera.look_at((0, 0, 0))
+    self.add(camera)
+    self.scene.camera = camera
+
+  def get_random_object(self):
+    random_id = self.rnd.choice([
+        "LargeMetalCube",
+        "LargeMetalCylinder",
+        "LargeMetalSphere",
+        "LargeRubberCube",
+        "LargeRubberCylinder",
+        "LargeRubberSphere",
+        "MetalSpot",
+        "RubberSpot",
+        "SmallMetalCube",
+        "SmallMetalCylinder",
+        "SmallMetalSphere",
+        "SmallRubberCube",
+        "SmallRubberCylinder",
+        "SmallRubberSphere",
+    ])
+    if "Metal" in random_id:
+      material = kb.PrincipledBSDFMaterial(
+            color=kb.random_hue_color(rnd=self.rnd),
+            roughness=0.2,
+            metallic=1.0,
+            ior=2.5)
+    else:  # if "Rubber" in random_id:
+      material = kb.PrincipledBSDFMaterial(
+          color=kb.random_hue_color(rnd=self.rnd),
+          roughness=0.7,
+          specular=0.33,
+          metallic=0.,
+          ior=1.25)
+    return self.create_asset("KLEVR", asset_id=random_id, material=material)
+
+  def run(self):
+    self.add_camera()
+    self.add_lights()
+    self.add_floor()
+
+    spawn_region = [(-4, -4, 0), (4, 4, 3)]
+    velocity_range = [(-4, -4, 0), (4, 4, 0)]
+    objects = []
+
+    nr_objects = self.rnd.randint(self.config.min_nr_objects, self.config.max_nr_objects)
+    for i in range(nr_objects):  # random number of objects
+      obj = self.get_random_object()
+      self.place_without_overlap(obj, [kb.rotation_sampler(),
+                                       kb.position_sampler(spawn_region)])
+      obj.velocity = (self.rnd.uniform(*velocity_range) -
+                      [obj.position[0], obj.position[1], 0])  # bias velocity towards center
+      objects.append(obj)
+
+    sim_path = self.save_simulator_state()
+    self.run_simulation()
+    render_path = self.save_renderer_state()
+    self.render()
+    output = self.post_process()
+    # collect ground-truth factors
+    output["factors"] = []
+    for i, obj in enumerate(objects):
+      output["factors"].append({
+          "asset_id": obj.asset_id,
+          "material": "Metal" if "Metal" in obj.asset_id else "Rubber",
+          "mass": obj.mass,
+          "color": obj.material.color.rgb,
+          "animation": obj.keyframes,
+      })
+    out_path = self.save_output(output)
+    name = datetime.datetime.now().strftime("%b%d_%H-%M-%S")
+    self.export(self.output_dir, name, files_list=[sim_path, render_path, out_path])
+
+
+if __name__ == '__main__':
+    worker = KLEVRWorker()
+
+    worker.setup()
+    worker.run()
---
+++ kubric/random.py
@@ -0,0 +1,80 @@
+# Copyright 2020 The Kubric Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from kubric import color
+import numpy as np
+import mathutils
+from kubric import core
+import itertools
+
+mathutils.Quaternion()
+default_random_state = np.random.RandomState()
+
+
+def random_hue_color(saturation: float = 1., value: float = 1, rnd=default_random_state):
+  return color.Color.from_hsv(rnd.random_sample(), saturation, value)
+
+
+def random_rotation(axis=None, rnd=default_random_state):
+  """ Compute a random rotation as a quaternion.
+  If axis is None the rotation is sampled uniformly over all possible orientations.
+  Otherwise it corresponds to a random rotation around the given axis."""
+
+  if axis is None:
+    # uniform over all possible orientations
+    z = 2
+    while z > 1:
+      x, y = rnd.rand(2)
+      z = x*x + y*y
+
+    w = 2
+    while w > 1:
+      u, v = rnd.rand(2)
+      w = u*u + v*v
+
+    s = np.sqrt((1-z) / w)
+    return x, y, s*u, s*v
+  else:
+    if isinstance(axis, str) and axis.upper() in ["X", "Y", "Z"]:
+      axis = {"X": (1., 0., 0., 0.),
+              "Y": (0., 1., 0.),
+              "Z": (0., 0., 1.)}[axis.upper()]
+
+    quat = mathutils.Quaternion(axis, rnd.uniform(0, 2*np.pi))
+    return tuple(quat)
+
+
+def rotation_sampler(axis=None):
+  def _sampler(obj: core.PhysicalObject, rnd):
+    obj.quaternion = random_rotation(axis=axis, rnd=rnd)
+  return _sampler
+
+
+def position_sampler(region):
+  region = np.array(region, dtype=np.float)
+
+  def _sampler(obj: core.PhysicalObject, rnd):
+    # make a copy of the bbox points in the matutils.Vector format
+    bounds = np.array(obj.bounds, dtype=np.float)
+    bbox_points = [mathutils.Vector(x)
+                   for x in itertools.product(bounds[:, 0], bounds[:, 1], bounds[:, 2])]
+    obj_orientation = mathutils.Quaternion(obj.quaternion)
+    for x in bbox_points:
+      x.rotate(obj_orientation)  # rotates x in place by the object orientation
+    bbox_points = np.array([tuple(x) for x in bbox_points])  # convert to np.array
+    rotated_bounds = np.array([bbox_points.min(axis=0), bbox_points.max(axis=0)])
+
+    effective_region = np.array(region) - rotated_bounds
+    obj.position = rnd.uniform(*effective_region)
+
+  return _sampler
