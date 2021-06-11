ddae3af570ac066966a0e08f7f75052c83bf0af9
Andrea Tagliasacchi <atagliasacchi@google.com>
2021-06-11 10:17:41 -0700

    Resolving issue114: base.py → assets.py  (#116)
    * renamed to asset.py for easy lookup
    
    * Undefined→UndefinedAssets
    
    * dropped helloworld3.py, added makefile for simulator

--- kubric/core/cameras.py
+++ kubric/core/cameras.py
@@ -15,7 +15,7 @@
 import numpy as np
 import traitlets as tl
 
-from kubric.core import base
+from kubric.core.assets import UndefinedAsset
 from kubric.core import objects
 
 __all__ = ("Camera", "UndefinedCamera", "PerspectiveCamera", "OrthographicCamera")
@@ -32,7 +32,7 @@ class Camera(objects.Object3D):
     return True
 
 
-class UndefinedCamera(Camera, base.Undefined):
+class UndefinedCamera(Camera, UndefinedAsset):
   """ Marker object that indicates that a camera instance attribute has not been set. """
   pass
 
