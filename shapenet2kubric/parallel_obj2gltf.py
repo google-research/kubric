1940053e449db35376bc82f55ee36c274ed9bce5
Andrea Tagliasacchi <atagliasacchi@google.com>
2021-07-29 11:15:31 -0700

    parsing of obj2gltf errors
    ...and ability to launch on a single object (debug problematic ones)

--- shapenet2kubric/parallel_obj2gltf.py
+++ shapenet2kubric/parallel_obj2gltf.py
@@ -1,6 +1,4 @@
-import fcntl
-import time
-import os
+# pylint disable=logging-fstring-interpolation
 import argparse
 import pathlib
 import multiprocessing
@@ -55,24 +53,32 @@ def shapenet_objects_dirs(datadir):
 # ------------------------------------------------------------------------------
 
 def process_object(object_folder):
+  object_folder = pathlib.Path(object_folder)
+
   # TODO: add option to delete file if exists?
   # see: https://docs.python.org/3/library/subprocess.html
   source_path = object_folder / "models" / "model_normalized.obj"
   target_path = object_folder / "models" / "model_normalized.glb"
   if not source_path.is_file():
     logger.error(f'The source path "{source_path}" is not a file?')
-  logger.debug(f'conversion of "{object_folder}" started...')
+  logger.debug(f'conversion of "{object_folder}"')
   cmd = f'obj2gltf -i {source_path} -o {target_path}'.split(' ')
   retobj = subprocess.run(cmd, capture_output=True, check=True)
-  if retobj.stdout != '':
-    if retobj.stdout.startswith(b"Total: "):
-      logger.debug(f'{retobj.stdout}')
-    else:
-      logger.error(f'{retobj.stdout}')
+  
+  # --- catch errors in stdout
+  stdout = retobj.stdout.decode('utf-8')
+  silent_error = not (stdout.startswith('Total') and stdout.endswith('ms\n'))
+  if silent_error:
+    logger.error(f'{stdout}')
+  else:
+    logger.debug(f'{stdout}') 
+
+  # --- catch errors in return code
   if retobj.returncode != 0:
-    if retobj.stderr != "": logger.debug(retobj.stderr)
+    if retobj.stderr != "": logger.debug(retobj.stderr.decode('utf-8'))
     logger.error(f'obj2gltf on "{target_path}" did not execute correctly.')
-  logger.debug(f'conversion of "{object_folder}" finished')
+
+  # --- verify post-hypothesis
   if not target_path.is_file():
     logger.error(f'The output "{target_path}" was not written.')
 
@@ -80,23 +86,32 @@ def process_object(object_folder):
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
+def parallel_launch(datadir, num_processes):
+  # --- gathers collection over which threads will be executed
+  object_folders = shapenet_objects_dirs(datadir)
+
+  # --- launches jobs in parallel
+  with tqdm.tqdm(total=len(object_folders)) as pbar:
+    with multiprocessing.Pool(num_processes) as pool:
+      for counter, _ in enumerate(pool.imap(process_object, object_folders)):
+        pbar.update(1)
+        logger.debug(f"Processed {counter}/{len(object_folders)}")
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--datadir', default='/ShapeNetCore.v2')
   parser.add_argument('--num_processes', default=8, type=int)
   args = parser.parse_args()
-  setup_logging(args.datadir)
 
-  # --- gathers collection over which threads will be executed
-  object_folders = shapenet_objects_dirs(args.datadir)
-
-  # --- launches jobs in parallel
-  with tqdm.tqdm(total=len(object_folders)) as pbar:
-    with multiprocessing.Pool(args.num_processes) as pool:
-      for counter, _ in enumerate(pool.imap(process_object, object_folders)):
-        pbar.update(1)
-        logger.debug(f"Processed {counter}/{len(object_folders)}")
-
+  # --- choose what to execute
+  if True:
+    setup_logging(args.datadir)
+    parallel_launch(args.datadir, args.num_processes)
+ 
+  if False:
+    # --- this is a problematic one
+    setup_logging(pathlib.Path.cwd())
+    process_object('/ShapeNetCore.v2/02691156/d583d6f23c590f3ec672ad25c77a396')
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
