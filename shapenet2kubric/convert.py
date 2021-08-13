5d87b9e59f99d766cbc42d15ab85b070c6219259
Klaus Greff <klausg@google.com>
2021-08-13 14:54:01 +0200

    capture all the output from the blender conversion stage

--- shapenet2kubric/convert.py
+++ shapenet2kubric/convert.py
@@ -161,37 +161,37 @@ def stage35(object_folder: Path, logger=_DEFAULT_LOGGER):
   with RedirectStream(stream=sys.stdout, filename=str(log_path)):
     bpy.ops.import_scene.gltf(filepath=str(source_path), loglevel=50)
 
-  bpy.ops.object.select_all(action='DESELECT')
+    bpy.ops.object.select_all(action='DESELECT')
 
-  for obj in bpy.data.objects:
-    # remove duplicate vertices
-    bpy.context.view_layer.objects.active = obj
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.remove_doubles(threshold=1e-06)
-    bpy.ops.object.mode_set(mode='OBJECT')
-    # disable auto-smoothing
-    obj.data.use_auto_smooth = False
-    # split edges with an angle above 70 degrees (1.22 radians)
-    m = obj.modifiers.new("EdgeSplit", "EDGE_SPLIT")
-    m.split_angle = 1.22173
-    bpy.ops.object.modifier_apply(modifier="EdgeSplit")
-    # move every face an epsilon in the direction of its normal, to reduce clipping artifacts
-    m = obj.modifiers.new("Displace", "DISPLACE")
-    m.strength = 0.00001
-    bpy.ops.object.modifier_apply(modifier="Displace")
+    for obj in bpy.data.objects:
+      # remove duplicate vertices
+      bpy.context.view_layer.objects.active = obj
+      bpy.ops.object.mode_set(mode='EDIT')
+      bpy.ops.mesh.remove_doubles(threshold=1e-06)
+      bpy.ops.object.mode_set(mode='OBJECT')
+      # disable auto-smoothing
+      obj.data.use_auto_smooth = False
+      # split edges with an angle above 70 degrees (1.22 radians)
+      m = obj.modifiers.new("EdgeSplit", "EDGE_SPLIT")
+      m.split_angle = 1.22173
+      bpy.ops.object.modifier_apply(modifier="EdgeSplit")
+      # move every face an epsilon in the direction of its normal, to reduce clipping artifacts
+      m = obj.modifiers.new("Displace", "DISPLACE")
+      m.strength = 0.00001
+      bpy.ops.object.modifier_apply(modifier="Displace")
 
-  # join all objects together
-  bpy.ops.object.select_all(action='SELECT')
-  bpy.ops.object.join()
+    # join all objects together
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.join()
 
-  # set the name of the asset
-  bpy.context.active_object.name = asset_id
+    # set the name of the asset
+    bpy.context.active_object.name = asset_id
 
-  # rename the source file
-  source_path.rename(source_backup_path)
+    # rename the source file
+    source_path.rename(source_backup_path)
 
-  # store new visual geometry
-  bpy.ops.export_scene.gltf(filepath=str(target_path), check_existing=True)
+    # store new visual geometry
+    bpy.ops.export_scene.gltf(filepath=str(target_path), check_existing=True)
 
   if not target_path.is_file():
     logger.error(f'stage3 post-condition failed, file does not exist "{target_path}"')
