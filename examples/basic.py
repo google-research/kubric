74f5f2beb4566c20f25fe35d217fc61fbbda20ef
Andrea Tagliasacchi <atagliasacchi@google.com>
2021-08-17 21:45:11 -0700

    attempt (3) to trigger nightly workflow

--- examples/basic.py
+++ examples/basic.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""A simple example w/o advanced dependencies."""
+"""A simple example w/o advanced (bullet/blender) dependencies."""
 
 import logging
 import kubric as kb
