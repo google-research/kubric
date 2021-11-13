1b8f34fbdc2e571d8870dd8b1c5cbf384cf96759
Andea Tagliasacchi <atagliasacchi@google.com>
2021-11-13 04:07:39 +0000

    launched single instance remotely (test)

---
+++ colmap/launch.sh
@@ -0,0 +1,30 @@
+# WARNING: verify credentials are enabled "gcloud auth configure-docker"
+# WARNING: verify credentials are enabled "gcloud auth application-default login"
+
+PROJECT_ID="kubric-xgcp"
+REGION="us-central1"  #< WARNING: match region of bucket!
+JOB_NAME="colmap_`date +"%b%d_%H%M%S"`"
+
+# --- Container configuration
+cat > Dockerfile <<EOF
+FROM kubricdockerhub/colmap:latest
+COPY script.sh /workspace/script.sh
+WORKDIR /workspace
+ENTRYPOINT ["/bin/bash", "script.sh"]
+EOF
+
+# --- Parameters for the launch
+TAG="gcr.io/$PROJECT_ID/colmap"
+docker build -f Dockerfile -t $TAG $PWD
+docker push $TAG
+
+# --- Launches the job on aiplatform
+gcloud beta ai-platform jobs submit training $JOB_NAME \
+  --region $REGION \
+  --scale-tier custom --master-machine-type standard_v100 \
+  --master-image-uri $TAG \
+  -- \
+  gs://kubric-public/colmap/input/semistatic1 \
+  gs://kubric-public/colmap/output/semistatic1
+
+gcloud ai-platform jobs stream-logs $JOB_NAME
--- colmap/script.sh
+++ colmap/script.sh
@@ -1,20 +1,31 @@
-IMAGE_PATH=/workspace/semistatic
-OUTPUT_PATH=/workspace/semistatic_output
-mkdir $OUTPUT_PATH
+#!/bin/bash
+
+# --- test whether container nvidia-docker worked
+# nvidia-smi && exit
+
+echo "finding images from: '$1'" 
+echo "storing colmap output: '$2'"
+
+# --- sets up tmp directories
+mkdir -p /tmp/input
+mkdir -p /tmp/output
+IMAGE_PATH="/tmp/input"
+OUTPUT_PATH="/tmp/output"
 
 # --- copy input data in a local directory
-gsutil -m cp -r gs://kubric-public/colmap/input/semistatic1 /tmp/semistatic1
+gsutil -m cp -r $1/rgba_*.png /tmp/input
 
-# export QT_QPA_PLATFORM=offscreen
+# --- avoids error for headless execution
+export QT_QPA_PLATFORM=offscreen
 colmap feature_extractor \
   --image_path $IMAGE_PATH \
   --database_path $OUTPUT_PATH/database.db \
   --ImageReader.single_camera 1 \
-  --SiftExtraction.use_gpu 0
+  --SiftExtraction.use_gpu 1
 
 colmap exhaustive_matcher \
   --database_path $OUTPUT_PATH/database.db \
-  –-SiftMatching.use_gpu 0
+  –-SiftMatching.use_gpu 1
 
 colmap mapper \
   --image_path $IMAGE_PATH \
@@ -23,4 +34,7 @@ colmap mapper \
   --Mapper.num_threads 16 \
   --Mapper.init_min_tri_angle 4\
   --Mapper.multiple_models 0\
-  --Mapper.extract_colors 0\
+  --Mapper.extract_colors 0
+
+# --- copy results back to bucket
+gsutil -m cp -r /tmp/output $2 
