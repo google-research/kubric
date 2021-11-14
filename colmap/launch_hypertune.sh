1c1b4d794ca65c3c2490bbffc6441610b0a4b570
Andea Tagliasacchi <atagliasacchi@google.com>
2021-11-14 05:55:33 +0000

    scheduled @ 50x P100

--- colmap/launch_hypertune.sh
+++ colmap/launch_hypertune.sh
@@ -19,16 +19,16 @@ cat > /tmp/hypertune.yml << EOF
     hyperparameters:
       goal: MAXIMIZE
       hyperparameterMetricTag: "answer"
-      maxTrials: 4
-      maxParallelTrials: 4
-      maxFailedTrials: 4
+      maxTrials: 599
+      maxParallelTrials: 50
+      maxFailedTrials: 599
       enableTrialEarlyStopping: False
       # --- each of these become an argparse argument
       params:
       - parameterName: sceneid
         type: INTEGER
         minValue: 0
-        maxValue: 4
+        maxValue: 599
 EOF
 
 # --- Parameters for the launch
