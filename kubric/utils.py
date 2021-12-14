58d1193f86fda65fb304fa5e4aebdbddfffb6a37
Andrea Tagliasacchi <atagliasacchi@google.com>
2021-12-13 17:17:11 -0800

    bug fixed

--- kubric/utils.py
+++ kubric/utils.py
@@ -11,20 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Copyright 2021 The Kubric Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from absl.flags import argparse_flags
 import argparse
 import collections
 import copy
@@ -51,11 +38,11 @@ logger = logging.getLogger(__name__)
 # Kubric argparser
 # --------------------------------------------------------------------------------------------------
 
-class ArgumentParser(argparse.ArgumentParser):
+class ArgumentParser(argparse_flags.ArgumentParser):
   """An argumentparser with default options, and compatibility with the Blender REPL."""
 
   def __init__(self, *args, **kwargs):
-    argparse.ArgumentParser.__init__(self, *args, **kwargs)
+    super().__init__(*args, **kwargs)
 
     # --- default arguments for kubric
     self.add_argument("--frame_rate", type=int, default=24,
