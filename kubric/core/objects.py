1544a858a3e17548dd7fdf5bbab3ff35485972be
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2022-04-20 03:08:00 +0300

    Revert "bug fix w.r.t. lookat (#198)"
    This reverts commit a3d5eaa073c451bfe7797acf2c5235c6958d855e.
    This is because on the new blender 3.1.2, with this patch applied,
    the camera is rotated with its up side pointing to the right.

--- kubric/core/objects.py
+++ kubric/core/objects.py
@@ -89,8 +89,8 @@ def look_at_quat(
 
   # construct the desired coordinate basis front, right, up
   look_at_front = normalize(target - position)
-  look_at_right = normalize(np.cross(up, look_at_front), fallback=world_right)
-  look_at_up = normalize(np.cross(look_at_front, look_at_right), fallback=world_up)
+  look_at_right = normalize(np.cross(world_up, look_at_front), fallback=world_right)
+  look_at_up = normalize(np.cross(look_at_front, look_at_right))
 
   rotation_matrix1 = np.stack([look_at_right, look_at_up, look_at_front])
   rotation_matrix2 = np.stack([right, up, front])
