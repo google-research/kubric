e140e24e078d5e641c4ac10bf25743059bd059ce
Philipp Henzler <philipphenzler14@gmail.com>
2023-10-16 09:49:59 -0700

    Allow kubric to set principal point shift for blender camera. (#306)
    Note that this cl, however, does not handle the support for principal points in kubric.
    
    PiperOrigin-RevId: 568999187
    
    Co-authored-by: kubric-team <kubric@google.com>

--- shapenet2kubric/trimesh_utils.py
+++ shapenet2kubric/trimesh_utils.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2023 The Kubric Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#!/usr/bin/env python3
 
 import json
 import sys
