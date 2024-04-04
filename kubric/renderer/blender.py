7a5a2b912cc8afc012db70059c3a1396be0c0bc3
Ricardo Martin Brualla <rmbrualla@google.com>
2024-04-04 03:48:55 -0700

    Switches singledispatchmethod to functools' implementation. (#315)
    PiperOrigin-RevId: 619431393
    
    Co-authored-by: Klaus Greff <klausg@google.com>

--- kubric/renderer/blender.py
+++ kubric/renderer/blender.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pylint: disable=function-redefined (removes singledispatchmethod pylint errors)
-
 import collections
 from contextlib import redirect_stdout
 import functools
