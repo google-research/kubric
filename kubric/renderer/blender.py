d9b547e7ae396ce6f128e9f7544f7469203f068d
Matthew Prasad Burruss <burrussmatthew@gmail.com>
2024-11-28 23:16:21 -0500

    Remove space

--- kubric/renderer/blender.py
+++ kubric/renderer/blender.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import collections
 from contextlib import redirect_stdout
 import functools
